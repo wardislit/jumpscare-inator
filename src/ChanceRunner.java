40f3a532f1a8bca7a88bbc06b27c2f43c783cef9
wardislit <lauriojohnedward777@gmail.com>
2026-01-22 17:43:05 +0800

    Added notes, and making exe file

--- src/ChanceRunner.java
+++ src/ChanceRunner.java
@@ -3,6 +3,9 @@ package src;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 import javax.swing.SwingUtilities;
 
@@ -12,7 +15,34 @@ public class ChanceRunner {
     @SuppressWarnings("unused")
     private static FoxyWindow jumpscare;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        if (!SystemTray.isSupported()) {
+            System.out.println("System tray not supported");
+            return;
+        }
+
+        SystemTray tray = SystemTray.getSystemTray();
+
+        // Icon image (use small 16x16 or 32x32 PNG)
+        Image image = Toolkit.getDefaultToolkit().getImage("icon.png");
+
+        // Tooltip text
+        TrayIcon trayIcon = new TrayIcon(image, "For those who know:");
+
+        trayIcon.setImageAutoSize(true);
+
+        // Popup menu
+        PopupMenu menu = new PopupMenu();
+        MenuItem exitItem = new MenuItem("Exit");
+
+        menu.add(exitItem);
+
+        trayIcon.setPopupMenu(menu);
+
+        // Add to system tray
+        tray.add(trayIcon);
+
+        // Actions
         ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
         scheduler.scheduleAtFixedRate(() -> {
@@ -20,6 +50,7 @@ public class ChanceRunner {
                 triggerEvent();
             }
         }, 0, 1, TimeUnit.SECONDS);
+        exitItem.addActionListener(e -> System.exit(0));
     }
 
     private static void triggerEvent() {
